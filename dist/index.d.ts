b01e60e5eb5e4157b52abf64fa19ea319693958c
suxiaomo <lksuxiaomo@outlook.com>
2023-04-05 21:47:09 +0800

    清除

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,7 +4,24 @@
 @Author  :   路拾遗
 @Version :   3.0.2
 @Contact :   lk920125@hotmail.com
-yarn add axios @rematch/loading @rematch/core  react-redux redux typescript @tarojs/taro
+yarn add @rematch/loading @rematch/core  react-redux redux typescript
 ***************************************/
-export * from './dva';
-export * from './request';
+export declare enum EFunctionKey {
+    RSetState = "RSetState",
+    EGet = "EGet",
+    EGetOne = "EGetOne",
+    EPost = "EPost",
+    EPostBatch = "EPostBatch",
+    EDelete = "EDelete",
+    EPut = "EPut",
+    EPutBatch = "EPutBatch"
+}
+export declare const reducers: {
+    RSetState(state: any, payload: any): any;
+};
+export declare function bindingModel(model: any): void;
+export declare const initModels: (printLog?: boolean, token?: string) => any;
+export declare const useLoading: (namespace: string) => any;
+export declare const useConnect: (namespace: string) => any;
+export declare const reducer: (namespace: string, type: string, payload: any) => any;
+export declare const effect: (namespace: string, type: string, payload?: any) => Promise<any>;
--- dist/request.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function initRequest(serverHome: string[], errorHandler: Function, printLog?: boolean, serverHomeIndex?: number, token?: string): void;
-export declare function bindHeader(key: string, value: string): void;
-export declare function getUrl(url?: string, index?: number): string;
-export declare function requestGet(url: string, body?: any, serverHomeIndex?: number): Promise<any>;
-export declare function requestDelete(url: string, serverHomeIndex?: number): Promise<any>;
-export declare function requestPost(url: string, body?: any, serverHomeIndex?: number): Promise<any>;
-export declare function requestPatch(url: string, body?: any, serverHomeIndex?: number): Promise<any>;
-export declare function requestPut(url: string, body?: any, serverHomeIndex?: number): Promise<any>;
-export declare function bindJWTToken(token?: string): void;
